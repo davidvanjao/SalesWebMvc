d81d1d1ed9a07eed98ca3145024077a00da225e5
David Vanjao <davidrafaeldj@hotmail.com>
2025-04-03 04:40:51 -0300

    Implementando o Services

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context; //verificar importacao
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList(); //acessa os dados ta tabela vendedores e converte em lista. Operacao Sincrona. 
+        }
+    }
+}
